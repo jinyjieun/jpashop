d81733ee4a0666f35d285beebb18d98832fcf2ef
jinyjieun <kyunmho@gmail.com>
2021-04-22 22:52:57 +0900

    210422 애플리케이션 아키텍처, 회원 리포지토리/서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,9 +1,11 @@
-package jpabook.jpashop.domain;
+package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -15,16 +17,27 @@ public class MemberRepository {
 
     @PersistenceContext
     private EntityManager em;
-    //엔티티 매니저 생성하는거는 부트가 알아서 해줌
+    //엔티티 매니저 생성하는거는 부트가 알아서 해서 주입해줌.
 
     //커맨드와 쿼리를 분리해라.
     //아래는 사이드 이펙트를 일으키는 커맨드이기 때문에 저장을 하고 나면 가급적이면 리턴값을 만들지 않음.
-    public Long save(Member member) {
+    //persist 하면 영속성컨텍스트에 멤버 엔티티를 넣고 트랜잭션이 커밋되는 시점에 디비에 반영됨.
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
